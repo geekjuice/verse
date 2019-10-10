950347ac209310586e5081361c54566f10a8e6f1
Nicholas Hwang <hello@undefined.engineer>
2019-10-10 19:30:03 -0400

    add padding to lyrics

--- src/prettify.ts
+++ src/prettify.ts
@@ -4,8 +4,14 @@ const { cyan, magenta } = chalk;
 
 const NEWLINE = '\n';
 
+const pad = (message: string): string => `  ${message}`;
+
 const format = (lyrics: string): string =>
   lyrics.trim().replace(/\[.+\]/g, match => magenta(match));
 
 export default (title: string, lyrics: string): string =>
-  [NEWLINE, cyan(title), NEWLINE, format(lyrics)].join(NEWLINE);
+  [NEWLINE, cyan(title), NEWLINE, format(lyrics)]
+    .join(NEWLINE)
+    .split(NEWLINE)
+    .map(pad)
+    .join(NEWLINE);
